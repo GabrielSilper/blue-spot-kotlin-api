515d7473b9ee6e918d00d4bc2d4d5ca2311580a0
GabrielSilper <gabrielsilper.main@gmail.com>
2023-10-21 01:06:48 -0400

    test: Adicionei as nomeações dos casos de testes de LandlordService

---
+++ src/test/kotlin/org/pdm/bluespot/unit/services/LandlordServiceTests.kt
@@ -0,0 +1,30 @@
+package org.pdm.bluespot.unit.services
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LandlordServiceTests {
+
+    @Test
+    fun `should create a new landlord`() {
+
+    }
+
+    @Test
+    fun `should provide a specific landlord`() {
+
+    }
+
+    @Test
+    fun `should register a new property for a landlord`() {
+
+    }
+
+    @Test
+    fun `should provide all properties of a landlord`() {
+
+    }
+
+}

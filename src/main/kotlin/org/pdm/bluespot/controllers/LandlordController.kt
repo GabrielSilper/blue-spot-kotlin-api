bcd437b580d02fb91d07dce5c481627652029405
GabrielSilper <gabrielsilper.main@gmail.com>
2023-10-21 00:01:55 -0400

    feat: Rota POST para registrar uma propriedade para um locador.

--- src/main/kotlin/org/pdm/bluespot/controllers/LandlordController.kt
+++ src/main/kotlin/org/pdm/bluespot/controllers/LandlordController.kt
@@ -1,10 +1,12 @@
 package org.pdm.bluespot.controllers
 
 import org.pdm.bluespot.applications.contracts.ILandlordService
-import org.pdm.bluespot.core.dtos.ResponseMessage
+import org.pdm.bluespot.core.dtos.PropertyCreationDto
 import org.pdm.bluespot.core.dtos.UserCreationDto
+import org.pdm.bluespot.core.entities.Property
 import org.pdm.bluespot.core.entities.users.Landlord
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,10 +19,15 @@ class LandlordController(private val landlordService: ILandlordService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createLandlord(@RequestBody data: UserCreationDto): ResponseMessage<Landlord> {
-        val newLandlord = this.landlordService.createLandlord(data.toLandlord())
-        val message = "Landlord created successfully"
-        return ResponseMessage(message, newLandlord)
+    fun createLandlord(@RequestBody data: UserCreationDto): Landlord {
+        return landlordService.createLandlord(data.toLandlord())
     }
 
+    @PostMapping("/{landlordId}/properties")
+    fun registerProperty(
+        @PathVariable landlordId: String,
+        @RequestBody data: PropertyCreationDto
+    ): Property {
+        return landlordService.registerProperty(landlordId, data.toProperty())
+    }
 }

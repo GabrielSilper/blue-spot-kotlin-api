926251d02bc69e8f4d2d5621b60d18dbe29e5f3d
GabrielSilper <gabrielsilper.main@gmail.com>
2023-10-20 23:22:21 -0400

    feat: adicionei e implementei a função de registrar uma propriedade.

--- src/main/kotlin/org/pdm/bluespot/applications/contracts/ILandlordService.kt
+++ src/main/kotlin/org/pdm/bluespot/applications/contracts/ILandlordService.kt
@@ -1,8 +1,10 @@
 package org.pdm.bluespot.applications.contracts
 
+import org.pdm.bluespot.core.entities.Property
 import org.pdm.bluespot.core.entities.users.Landlord
 
 interface ILandlordService {
     fun createLandlord(data: Landlord): Landlord
     fun getLandlordById(id: String): Landlord
+    fun registerProperty(landlordId: String, property: Property): Property
 }
--- src/main/kotlin/org/pdm/bluespot/services/LandlordService.kt
+++ src/main/kotlin/org/pdm/bluespot/services/LandlordService.kt
@@ -1,18 +1,32 @@
 package org.pdm.bluespot.services
 
 import org.pdm.bluespot.applications.contracts.ILandlordService
+import org.pdm.bluespot.applications.contracts.IPropertyService
+import org.pdm.bluespot.core.entities.Property
 import org.pdm.bluespot.core.entities.users.Landlord
 
-import org.pdm.bluespot.core.entities.users.Tenant
 import org.pdm.bluespot.core.exceptions.LandlordNotFoundException
 import org.pdm.bluespot.core.repositories.users.LandlordRepository
 
-class LandlordService(private val landlordRepository: LandlordRepository) : ILandlordService {
+class LandlordService(
+    private val landlordRepository: LandlordRepository,
+    private val propertyService: IPropertyService
+) : ILandlordService {
     override fun createLandlord(data: Landlord): Landlord {
         return this.landlordRepository.save(data)
     }
 
     override fun getLandlordById(id: String): Landlord {
         return this.landlordRepository.findById(id).orElseThrow { LandlordNotFoundException() }
+    }
+
+    override fun registerProperty(landlordId: String, property: Property): Property {
+        val landlord = this.getLandlordById(landlordId)
+        val newProperty = this.propertyService.createProperty(property)
+
+        landlord.properties.add(newProperty)
+        this.landlordRepository.save(landlord)
+
+        return newProperty
     }
 }

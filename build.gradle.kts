78d6304477a091f2655b407baa52114ba9e69d19
GabrielSilper <gabrielsilper.main@gmail.com>
2023-10-22 04:27:41 -0400

    test: implementei os testes para o arquivo LandolordService

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.mockito:mockito-core:5.2.0")
+	testImplementation("io.mockk:mockk:1.10.4")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/org/pdm/bluespot/mocks/propertyMocks.kt
@@ -0,0 +1,32 @@
+package org.pdm.bluespot.mocks
+
+import org.pdm.bluespot.core.dtos.PropertyCreationDto
+import org.pdm.bluespot.core.entities.Property
+import org.pdm.bluespot.core.enums.PropertyType
+
+
+val mockProperty: Property = PropertyCreationDto(
+    "Address",
+    "Description",
+    1000.0,
+    PropertyType.RESIDENCIAL,
+    mutableListOf("Proibido fumar"),
+    mutableListOf("Sofá"),
+    hasGarage = true
+).toProperty()
+
+val mockResultProperty = Property(
+    "653352ad7d213c17a1d6fte",
+    "653352ad7d213c17a1d0fc5e",
+    mutableListOf("image1", "image2"),
+    "Description",
+    "Address",
+    1000.0,
+    PropertyType.RESIDENCIAL,
+    mutableListOf("Proibido fumar"),
+    mutableListOf("Sofá"),
+    hasGarage = true,
+    isAvailable = true,
+    isVerified = false
+)
+
